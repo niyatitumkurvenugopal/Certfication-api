27f71e25d7791d15b051a3cf8f6d1503816a83c1
niyatitv <62688725+niyatitv@users.noreply.github.com>
2022-01-09 22:24:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Users from './components/Users.js'
+import Products from './components/Products';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Users/> */}
+      <Products/>
+      
+    </div>
+  );
+}
+
+export default App;
